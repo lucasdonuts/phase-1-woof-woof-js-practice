ea0c3cb37cd6ab3f9df5d3640abc61d40d04456a
Lucas Duncan <lucasdonuts@gmail.com>
2022-07-08 15:38:43 -0500

    Good/bad dog toggle button fully functional, also updates database

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
+// After getting it to work, go back and try a new method.
+// New method renders all dogs on page load, with display set to hidden.
+// When dog is selected, unhide that dog's info card.
+
 document.addEventListener('DOMContentLoaded', () => {
   // Constant declarations
   const baseUrl = 'http://localhost:3000/pups';
   const dogBar = document.querySelector('#dog-bar');
   const dogInfo = document.querySelector('#dog-info');
 
-  //Fetch pup data function
+  //Fetch functions
   function fetchPups() {
     return fetch(baseUrl)
     .then(res => res.json())
+  }
+
+  function updateDog(dogObj) {
+    fetch(baseUrl + `/${dogObj.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify(dogObj)
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
   }
 
   // Render functions
@@ -26,9 +43,10 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function renderDogInfo(dogObj) {
-    // img
-    // name h2
-    // good dog button
+    while (dogInfo.firstChild) {
+      dogInfo.firstChild.remove();
+    }
+
     const img = document.createElement('img');
     const name = document.createElement('h2');
     const button = document.createElement('button');
@@ -39,10 +57,16 @@ document.addEventListener('DOMContentLoaded', () => {
     button.textContent = dogObj.isGoodDog ? 'Good Dog!' : 'Bad Dog!';
 
     button.addEventListener('click', () => {
-      
+      button.textContent = dogObj.isGoodDog ? 'Bad Dog!' : 'Good Dog!';
+      dogObj.isGoodDog = !dogObj.isGoodDog;
+
+      updateDog(dogObj);
     })
+
+    dogInfo.append(img, name, button);
   }
 
+  // Load page content
   fetchPups()
     .then(pups => fillDogBar(pups));
 })
